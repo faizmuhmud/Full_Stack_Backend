9aec112b0ae3b3a7c8e10b902317027a8c627588
faizmuhmud <mohammedfaizv9@gmail.com>
2025-12-02 00:24:07 +0400

    Added CORS Middleware

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('Server is running');
+    res.send('Server is running with CORS enabled');
 });
  
 app.listen(port, () => {
